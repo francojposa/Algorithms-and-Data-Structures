b4e56752f591cb32b092714c1fc91b3cd2c303d7
francojposa <franco@francoposa.io>
2020-06-07 10:06:35 -0500

    Golang implementation of the slightly less naive max sum subarray

--- ch1-algorithm-analysis/max-sum-subarray/max_sum_subarray.go
+++ ch1-algorithm-analysis/max-sum-subarray/max_sum_subarray.go
@@ -1,12 +1,11 @@
 package algorithmanalysis
 
-// MaxSumSubArraySlow is a naive implementation of the Max Sum Subarray
-// Problem, running in O(n^3) time by enumerating all n^2 possible
-// subarrays and summing the m elements of each of those subarrays
+// MaxSumSubArraySlow is a naive implementation of the Max Sum Subarray Problem
+// running in O(n^3) time by enumerating all n^2 possible subarrays and summing
+// the m elements of each of those subarrays
 func MaxSumSubArraySlow(a []int) int {
-	// the base case subarray is empty with sum 0
-	max := 0
-	for startIndex := 0; startIndex < len(a); startIndex++ {
+	max := 0 // the base case subarray is empty with sum 0
+	for startIndex := range a {
 		// we will enumerate all subarrays that start at index 0,
 		// then all subarrays that start at index 1, etc...
 		for endIndex := startIndex + 1; endIndex < len(a); endIndex++ {
@@ -14,7 +13,7 @@ func MaxSumSubArraySlow(a []int) int {
 			// when start index is 1, we make subarrays a[1..1] up to a[1..len(a) - 1]
 			// and so on. There is no need to "look back" from a start index of 1
 			// to cover the subarray a[1..0] because a[0..1] was already covered on the
-			// previous outer loop iteration and order of elements does not matter
+			// previous outer loop iteration and order of elements does not matter for sums
 			sum := 0
 			for i := startIndex; i < endIndex; i++ {
 				// sum all elements a[start index...end index]
@@ -25,5 +24,39 @@ func MaxSumSubArraySlow(a []int) int {
 			}
 		}
 	}
+	return max
+}
+
+// MaxSumSubArrayFaster is a slightly-less naive implementation of the Max Sum Subarray Problem
+// running in O(n^2) time by iterating once to build an array s of the sums of a[0..i] for i < n,
+// then enumerating all n^2 possible subarrays, but calculating the sums in constant time by
+// using the array of sums: sum(a[i..j]) = s[j] - s[i]
+func MaxSumSubArrayFaster(a []int) int {
+	// build an array of the sums of a[0..i] for i < n
+	sums := make([]int, len(a))
+	cumulativeSum := 0
+	for i, v := range a {
+		cumulativeSum += v
+		sums[i] = cumulativeSum
+	}
+
+	max := 0 // the base case subarray is empty with sum 0
+
+	for startIndex := range a {
+		// we will enumerate all subarrays that start at index 0,
+		// then all subarrays that start at index 1, etc...
+		for endIndex := startIndex + 1; endIndex < len(a); endIndex++ {
+			// when start index is 0, we make subarrays a[0..0] up to a[0..len(a) - 1]
+			// when start index is 1, we make subarrays a[1..1] up to a[1..len(a) - 1]
+			// and so on. There is no need to "look back" from a start index of 1
+			// to cover the subarray a[1..0] because a[0..1] was already covered on the
+			// previous outer loop iteration and order of elements does not matter for sums
+			sum := sums[endIndex] - sums[startIndex]
+			if sum > max {
+				max = sum
+			}
+		}
+	}
+
 	return max
 }
--- ch1-algorithm-analysis/max-sum-subarray/max_sum_subarray_test.go
+++ ch1-algorithm-analysis/max-sum-subarray/max_sum_subarray_test.go
@@ -10,4 +10,7 @@ func TestMaxSumSubarray(t *testing.T) {
 	arr := []int{-2, -4, 3, -1, 5, 6, -7, -2, 4, -3, 2}
 	max := MaxSumSubArraySlow(arr)
 	assert.Equal(t, 13, max)
+
+	max = MaxSumSubArrayFaster(arr)
+	assert.Equal(t, 13, max)
 }
