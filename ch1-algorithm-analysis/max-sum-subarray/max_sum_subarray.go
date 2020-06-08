6aa3ebf4893dd9fd72b992ef0621ac85917ee9b9
francojposa <franco@francoposa.io>
2020-06-08 07:55:30 -0500

    WIP explaining optimal max sum subarray

--- ch1-algorithm-analysis/max-sum-subarray/max_sum_subarray.go
+++ ch1-algorithm-analysis/max-sum-subarray/max_sum_subarray.go
@@ -60,3 +60,95 @@ func MaxSumSubArrayFaster(a []int) int {
 
 	return max
 }
+
+// MaxSumSubArrayFastest is an optimized implementation of the Max Sum Subarray Problem
+// running in O(n) time by iterating once to build an array m of the maximum subarray sum
+// for subarrays of a[0..i] for i < n, then returning the maximum of those maximums
+// The constant-time determinate of the maximum subarray sum for all subarrays of a[0..i]
+// is enabled by the following observation:
+// if the maximum sum of all subarrays a[0..i] is less than a[i],
+// then a greater max sum subarray can be created just from a[i..i] without including any
+// previous elements.
+// To illustrate, use the array a = [1, -2, 1, 1, -1, 3]. Base case maximum sum is 0.
+// 1. subarray space is a[0..0] = [1]
+//		Max sum of all possible subarrays in this subarray space is 1.
+//		Array of max sums of subarray space is [1]
+// 2. subarray space is a[0..1] = [1, -2]
+//		Previous subarray's sum 1 plus -2 is -1, which is not greater than sum of an empty set, 0
+//		Empty subarray's sum 0 plus -2 is -2, which is not greater than sum of an empty set, 0
+//		You cannot improve on the previous subarray's sum by adding this element
+//		You cannot improve on an empty subarray's sum by adding this element
+//		Array of max sums of subarray space is [1, 0]
+// 3. subarray space is a[0..2] = [1, -2, 1]
+//		Previous subarray's sum 0 plus 1 is 1, which is greater than sum of an empty set, 0
+//		Empty subarray's sum 0 plus 1 is 1, which is greater than sum of an empty set, 0
+//		You can improve on the previous subarray's sum by adding this element
+//		You can improve on an empty subarray's sum by adding this element
+//		Array of max sums of subarray space is [1, 0, 1]
+// 4. subarray space is a[0..3] = [1, -2, 1, 1]
+//		Previous subarray's sum 1 plus 1 is 2, which is greater than sum of an empty set, 0
+//		Empty subarray's sum 0 plus 1 is 1, which is greater than sum of an empty set, 0
+//		You can improve on the previous subarray's sum by adding this element
+//		You can improve on an empty subarray's sum by adding this element
+//		Array of max sums of subarray space is [1, 0, 1, 2]
+// 4. subarray space is a[0..4] = [1, -2, 1, 1, -3]
+//		Previous subarray's sum 2 plus -3 is -1, which is not greater than sum of an empty set, 0
+//		Empty subarray's sum 0 plus -3 is -3, which is not greater than sum of an empty set, 0
+//		You cannot improve on the previous subarray's sum by adding this element
+//		You cannot improve on an empty subarray's sum by adding this element
+//		Array of max sums of subarray space is [1, 0, 1, 1, 0]
+// 4. subarray space is a[0..5] = [1, -2, 1, 1, -3, 3]
+//		Previous subarray's sum 0 plus 3 is 3, which is greater than sum of an empty set, 0
+//		Empty subarray's sum 0 plus 3 is 3, which is greater than sum of an empty set, 0
+//		You can improve on the previous subarray's sum by adding this element
+//		You can improve on an empty subarray's sum by adding this element
+//		Array of max sums of subarray space is [1, 0, 1, 1, 0, 3]
+
+// To illustrate, use the array a = [-2, -4, 3, -1, 5, 6, -7, -2, 4, -3, 2] Base case maximum sum is 0.
+// 1. subarray space is a[0..0] = [-2]
+//		The previous subarray sum 0, plus this element -2, has sum -2
+//		This can trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0]
+// 2. subarray space is a[0..1] = [-2, -4]
+//		The previous subarray sum 0, plus this element -4, has sum -4
+//		This can trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0]
+// 3. subarray space is a[0..2] = [-2, -4, 3]
+//		The previous subarray sum 0, plus this element 3, has sum 3
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3]
+// 4. subarray space is a[0..3] = [-2, -4, 3, -1]
+//		The previous subarray sum 3, plus this element -1, has sum 2
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2]
+// 5. subarray space is a[0..4] = [-2, -4, 3, -1, 5]
+//		The previous subarray sum 2, plus this element 5, has sum 7
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2, 7]
+// 6. subarray space is a[0..5] = [-2, -4, 3, -1, 5, 6]
+//		The previous subarray sum 7, plus this element 6, has sum 13
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2, 7, 13]
+// 7. subarray space is a[0..6] = [-2, -4, 3, -1, 5, 6, -7]
+//		The previous subarray sum 13, plus this element -7, has sum 6
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2, 7, 13, 6]
+// 8. subarray space is a[0..7] = [-2, -4, 3, -1, 5, 6, -7, -2]
+//		The previous subarray sum 6, plus this element -2, has sum 4
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2, 7, 13, 6, 4]
+// 9. subarray space is a[0..8] = [-2, -4, 3, -1, 5, 6, -7, -2, 4]
+//		The previous subarray sum 4, plus this element 4, has sum 8
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2, 7, 13, 6, 4, 8]
+// 10. subarray space is a[0..9] = [-2, -4, 3, -1, 5, 6, -7, -2, 4, -3]
+//		The previous subarray sum 8, plus this element -3, has sum 5
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2, 7, 13, 6, 4, 8, 5]
+// 11. subarray space is a[0..9] = [-2, -4, 3, -1, 5, 6, -7, -2, 4, -3]
+//		The previous subarray sum 8, plus this element -3, has sum 5
+//		This cannot trivially be improved upon by starting over with the empty set of sum 0
+//		Array of max sums of subarray space is [0, 0, 3, 2, 7, 13, 6, 4, 8, 5]
+func MaxSumSubArrayFastest(a []int) int {
+	return 0
+}
