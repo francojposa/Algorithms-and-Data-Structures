5e9ba777da0e3237db88a38b4aa6a37748935fbb
francoposa <franco@francoposa.io>
2021-05-19 17:04:43 -0500

    improving tests for python recursive general tree

--- ch02-basic-data-structures/trees/test_recursive_tree.py
+++ ch02-basic-data-structures/trees/test_recursive_tree.py
@@ -3,26 +3,37 @@ from recursive_tree import Tree, traverse_preorder
 
 
 def test_traverse_preorder_method():
+    t_section = Tree(value="section", children=[])
     t1 = Tree(value="1", children=[])
     t1a = Tree(value="1a", children=[])
     t1b = Tree(value="1b", children=[])
+    t2 = Tree(value="2", children=[])
+    t2a = Tree(value="2a", children=[])
     t1.children = [t1a, t1b]
+    t2.children = [t2a]
+    t_section.children = [t1, t2]
 
-    sections: List[str] = []
+    table_of_contents: List[str] = []
 
-    t1.traverse_preorder(lambda t: sections.append(t.value))
+    t_section.traverse_preorder(lambda t: table_of_contents.append(t.value))
 
-    assert sections == ["1", "1a", "1b"]
+    assert table_of_contents == ["section", "1", "1a", "1b", "2", "2a"]
 
 
 def test_traverse_preorder_function():
-    t1 = Tree(value="1", children=[])
-    t1a = Tree(value="1a", children=[])
-    t1b = Tree(value="1b", children=[])
-    t1.children = [t1a, t1b]
+    t_section = Tree(
+        value="section",
+        children=[
+            Tree(
+                value="1",
+                children=[Tree(value="1a", children=[]), Tree(value="1b", children=[])],
+            ),
+            Tree(value="2", children=[Tree(value="2a", children=[])]),
+        ],
+    )
+    table_of_contents: List[str] = []
 
-    sections: List[str] = []
+    traverse_preorder(t_section, lambda t: table_of_contents.append(t.value))
 
-    traverse_preorder(t1, lambda t: sections.append(t.value))
+    assert table_of_contents == ["section", "1", "1a", "1b", "2", "2a"]
 
-    assert sections == ["1", "1a", "1b"]
